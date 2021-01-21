6e4b12c4e7d8dcf328534fee8fa25b09f2eedbc3
noririco <nisanlevy666@gmail.com>
2021-01-21 21:17:15 +0200

    malki gayz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDtVjjEgHF1NHFaYWoOlLz-rxtgPKVvI7k',
+    authDomain: 'zamarot-38278.firebaseapp.com',
+    databaseURL:
+      'https://zamarot-38278-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'zamarot-38278',
+    storageBucket: 'zamarot-38278.appspot.com',
+    messagingSenderId: '239651725026',
+    appId: '1:239651725026:web:326b0b68527e67379d08bb',
+    measurementId: 'G-4QWJ185QTP',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDtVjjEgHF1NHFaYWoOlLz-rxtgPKVvI7k',
+    authDomain: 'zamarot-38278.firebaseapp.com',
+    databaseURL:
+      'https://zamarot-38278-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'zamarot-38278',
+    storageBucket: 'zamarot-38278.appspot.com',
+    messagingSenderId: '239651725026',
+    appId: '1:239651725026:web:326b0b68527e67379d08bb',
+    measurementId: 'G-4QWJ185QTP',
+  },
 };
 
 /*
